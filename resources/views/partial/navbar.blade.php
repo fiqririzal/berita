943795f36935bf40a3b34f38471ad2a7d2d64947
eryannaf <eryan.fajriani@gmail.com>
2022-10-15 23:00:12 +0700

    ecil - first

--- resources/views/partial/navbar.blade.php
+++ resources/views/partial/navbar.blade.php
@@ -10,5 +10,17 @@
     </ul>
 
     <!-- Right navbar links -->
-  
+        <!-- inject:js -->
+        <script src="assets/vendors/js/vendor.bundle.base.js"></script>
+        <!-- endinject -->
+        <!-- plugin js for this page -->
+        <script src="assets/vendors/aos/dist/aos.js/aos.js"></script>
+        <!-- End plugin js for this page -->
+        <!-- Custom js for this page-->
+        <script src="./assets/js/demo.js"></script>
+        <script src="./assets/js/jquery.easeScroll.js"></script>
+        <!-- End custom js for this page-->
+      </body>
+    </html>
+
   </nav>
